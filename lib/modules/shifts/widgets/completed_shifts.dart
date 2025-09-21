954d7fbf8a1d2952158eedc59e6ccf19fb2915ac
Sakhans0010 <54745117+Sakhans0010@users.noreply.github.com>
2025-09-21 05:47:09 +0100

    Added shift shimmer

--- lib/modules/shifts/widgets/completed_shifts.dart
+++ lib/modules/shifts/widgets/completed_shifts.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:kingdom_care_services_app/models/shift.dart';
 import 'package:kingdom_care_services_app/modules/home/widgets/date_timeline.dart';
-import 'package:kingdom_care_services_app/modules/shifts/widgets/shift_item_widget.dart';
+import 'package:kingdom_care_services_app/modules/shifts/widgets/shifts_list_shimmer.dart';
 
 class CompletedShifts extends StatelessWidget {
   const CompletedShifts({super.key});
@@ -15,28 +14,7 @@ class CompletedShifts extends StatelessWidget {
         children: [
           DateTimeline(),
           SizedBox(height: 20),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 10, // Example item count
-              itemBuilder: (context, index) {
-                return ShiftItemWidget(
-                  shift: ShiftItem(
-                    role: "Registered Nurse",
-                    title: "Shift at Woodford Hospital",
-                    startDateAndTime: DateTime.now(),
-                    endDateAndTime: DateTime.now().add(Duration(hours: 8)),
-                    notes: "Please arrive 15 minutes early.",
-                    location: Location(
-                      latitude: 51.4636868,
-                      longitude: -2.5686716,
-                      address: "29 Eve Road, Bristol, BS5 0JX",
-                    ),
-                    ratePerHour: "£20/hr",
-                  ),
-                );
-              },
-            ),
-          ),
+          Expanded(child: ShiftsListShimmer()),
         ],
       ),
     );
